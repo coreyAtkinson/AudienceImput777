be47fa2cdf199b298d1fa2b3c027e1632f2232d1
Olivia Mendez <olivianevaeh@yahoo.com>
2024-04-24 09:13:10 -0500

    added textfield and new view controller

---
+++ AudienceImput/ViewController22.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController22.swift
+//  AudienceImput
+//
+//  Created by OLIVIA MENDEZ on 4/24/24.
+//
+
+import UIKit
+
+class ViewController22: UIViewController {
+
+    @IBOutlet weak var textOutlet: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    @IBAction func buttonAction(_ sender: UIButton) {
+    }
+    
+}
